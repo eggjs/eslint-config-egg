931ed95c4786cb32dc50ddfc07c01ead4d7ca0dd
Benjy Cui <benjytrys@gmail.com>
2016-02-15 16:22:27 +0800

    update .eslintrc

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ describe('eslint', function() {
   it('should get rules', function() {
     assert.ok(eslintrc);
     assert.deepEqual(eslintrc.parser, 'espree');
-    assert.deepEqual(eslintrc.ecmaFeatures, {
+    assert.deepEqual(eslintrc.parserOptions.ecmaFeatures, {
       jsx: true,
     });
   });

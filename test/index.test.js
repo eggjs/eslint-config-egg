b14bd424795f5eb1f1e8f548a4c08fa62b0ef532
popomore <sakura9515@gmail.com>
2016-11-01 21:29:34 +0800

    feat: [BREAKING_CHANGE] enable prefer-arrow-callback

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,9 @@
 const assert = require('assert');
 const eslintrc = require('../index');
 
-describe('eslint', function() {
+describe('eslint', () => {
 
-  it('should get rules', function() {
+  it('should get rules', () => {
     assert.ok(eslintrc);
     assert.deepEqual(eslintrc.parser, 'espree');
     assert.deepEqual(eslintrc.parserOptions.ecmaFeatures, {

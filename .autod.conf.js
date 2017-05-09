3c102caaff09fcc26e124d8d963e46ed552ae02a
popomore <sakura9515@gmail.com>
2017-05-09 17:08:32 +0800

    deps: peerDeps to deps

--- .autod.conf.js
+++ .autod.conf.js
@@ -8,7 +8,13 @@ module.exports = {
     'test/fixtures',
     'examples',
     'benchmarks',
-    "docs",
+    'docs',
+  ],
+  dep: [
+    'babel-eslint',
+    'eslint-plugin-import',
+    'eslint-plugin-react',
+    'eslint-plugin-jsx-a11y',
   ],
   devdep: [
     'autod',

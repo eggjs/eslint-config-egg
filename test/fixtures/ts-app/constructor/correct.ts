0122bb4c7a59587f1d1a6ad702350c67a7fb5355
fengmk2 <fengmk2@gmail.com>
2025-01-22 00:11:03 +0800

    feat: allow empty constructors

--- test/fixtures/ts-app/constructor/correct.ts
+++ test/fixtures/ts-app/constructor/correct.ts
@@ -6,3 +6,22 @@ export class MyClass {
     // ignore
   }
 }
+
+export default class AppBoot {
+  constructor(
+    private readonly app: any,
+    private readonly config: any,
+  ) {}
+
+  public async didLoad() {
+    console.log(this.app, this.config);
+  }
+}
+
+export class AppBoot2 {
+  constructor(private readonly app: any) {}
+
+  public async didLoad() {
+    console.log(this.app);
+  }
+}
